1ce600333359007d422c80240dc7ab877c7660a6
yanxilu2 <yanxilu2@gmail.com>
2017-09-12 16:33:30 -0700

    update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,31 @@
 # Hint:  use Google to find python function
+from datetime import datetime
+####a)
+"""date_start = '01-02-2013'
+date_stop = '07-28-2015'
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_format = '%m-%d-%Y'
+a = datetime.strptime(date_start,date_format)
+b = datetime.strptime(date_stop,date_format)
+delta=b-a
+print(delta.days)
 
-####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+####b)
+date_start = '12312013'
+date_stop = '05282015'
 
-####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_format = '%m%d%Y'
+a = datetime.strptime(date_start,date_format)
+b = datetime.strptime(date_stop,date_format)
+delta=b-a
+print(delta.days)"""
+
+####c)
+date_start = '15-Jan-1994'
+date_stop = '14-Jul-2015'
+
+date_format = '%d-%b-%Y'
+a = datetime.strptime(date_start,date_format)
+b = datetime.strptime(date_stop,date_format)
+delta=b-a
+print(delta.days)
